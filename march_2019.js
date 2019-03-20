e157aac6e0ce358d6829339b585dc91bef05cc9b
Albert Garcia <albert.garcia10180@gmail.com>
2019-03-20 10:43:08 -0500

    added 3/20

--- march_2019.js
+++ march_2019.js
@@ -1,1 +1,17 @@
 // March Algorithems file
+// 3/20/2019
+// reverse each word in a string, returning a new string
+function reverseWords(str){
+    var arr = [];
+    arr = str.split(" ");
+    for(var i = 0; i < arr.length; i++){
+        var temp = "";
+        for(var j = arr[i].length - 1; j >= 0; j--){
+            temp += arr[i][j];
+        }
+        arr[i] = temp;
+    }
+    str = arr.join(" ");
+    return str;
+}
+console.log(reverseWords("the big blue house"));
