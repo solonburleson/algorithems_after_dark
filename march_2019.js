5e1a9298ea7ab9471bc5df4c71acf8cddabf363b
Albert Garcia <albert.garcia10180@gmail.com>
2019-03-28 12:19:52 -0500

    added 3/28

--- march_2019.js
+++ march_2019.js
@@ -32,3 +32,41 @@ function rvsWords2(str){
     return output;
 }
 console.log(rvsWords2("The Big Blue House"));
+
+// 3/28/2019
+// Community pool
+// find total amount of water that an arr can hold where values in arr 
+// are walls of the pools ex: [3,0,1,3,0,5] would hold 8 units of water. 
+// at arr[1] -> 3 units of water arr[2] -> 2 units arr[4] -> 3 units
+function comPool(arr){
+    var lw = arr[0];
+    var rw = 0;
+    var len = 0;
+    var deadSpace = 0;
+    var water = 0;
+    for(var i = 1; i <arr.length; i++){
+        if(arr[i] < lw){
+            deadSpace += arr[i];
+            len ++;
+        }
+        if(arr[i] >= lw){
+            water += len * lw - deadSpace;
+            len = 0;
+            deadSpace = 0;
+            rw = 0;
+            lw = arr[i];
+        }
+        if(arr[i] > rw && arr[i] < lw){
+            rw = arr[i];
+        }
+    }
+    if(rw < lw){
+        water += len * rw - deadSpace;
+    }
+    return water;
+}
+console.log(comPool([3,0,1,3,0,5]));
+console.log(comPool([3,1,3]));
+console.log(comPool([3,0,1,3,0,2]));
+console.log(comPool([3,0,1,3,0,0]));
+console.log(comPool([0,1,3,0,1,3,0,5]));
