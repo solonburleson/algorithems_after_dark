eaf8894c2e274c8d7d308c171e8aa767a3fab0ba
solonburleson <solon.burleson@gmail.com>
2019-04-10 11:08:30 -0500

    Dec to Apr upload

---
+++ solon_algos_dec_to_apr.js
@@ -0,0 +1,519 @@
+function URLify(str) {
+    var url = "";
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == " ") {
+            url += "%20";
+        }
+        else {
+            url += str[i];
+        }
+    }
+    return url;
+}
+
+URLify("He llo  ")-- > "He%20llo"
+
+var board = [[x, o, x],
+[o, x, o],
+[x, x, o]]
+
+function ticTacWin(board) {
+    var str = ""
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board[i].length; j + 2) {
+            if (board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2] && j == 0) {
+                str = "Winner is " + board[i][j]
+                return str
+            }
+            else if (board[i][j] == board[i + 1][j] && board[i][j] == board[i + 2][j] && i == 0) {
+                str = "Winner is " + board[i][j]
+                return str
+            }
+            else if (board[i][j] == board[i + 1][j + 1] && board[i][j] == board[i + 2][j + 2] && i == 0 && j == 0) {
+                str = "Winner is " + board[i][j]
+                return str
+            }
+            else if (board[i][j] == board[i - 1][j - 1] && board[i][j] == board[i - 2][j - 2] && i == 0 && j == 2) {
+                str = "Winner is " + board[i][j]
+                return str
+            }
+        }
+    }
+    return "No winner"
+}
+
+
+function sumSwap(arr1, arr2) {
+    var sum1 = 0;
+    var sum2 = 0;
+    var swaps = [];
+    var diff = 0;
+    for (var i = 0; i < arr1.length; i++) {
+        sum1 += arr1[i];
+    }
+    for (var i = 0; i < arr2.length; i++) {
+        sum2 += arr2[i];
+    }
+    if (sum1 != sum2) {
+        diff = sum1 - sum2;
+        if (diff % 2 != 0) {
+            return "Not able to swap to make equal"
+        }
+        diff /= 2;
+        for (var i = 0; i < arr1.length; i++) {
+            for (var j = 0; j < arr2.length; j++) {
+                if (diff == arr1[i] - arr2[j]) {
+                    swaps.push(arr1[i]);
+                    swaps.push(arr2[j]);
+                    return swaps
+                }
+            }
+        }
+    }
+    else {
+        return "No need to swap"
+    }
+}
+
+function sequence(arr) {
+    var sum = 0;
+    var max_sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (sum != 0) {
+            sum += arr[i];
+            if (max_sum < sum) {
+                max_sum = sum;
+            }
+            else if (sum < 0) {
+                sum = 0;
+            }
+        }
+        else {
+            sum += arr[i];
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+    }
+    return max_sum
+}
+
+
+function livingPeople(arr) {
+    var dict = {};
+    var year = 0;
+    var max = 0;
+    for (var i = 0; i < arr.length; i++) {
+        year = arr[i]['birth']
+        while (year <= arr[i]['death']) {
+            if (!(year in dict)) {
+                dict[year] = 1;
+            }
+            else {
+                dict[year] += 1;
+            }
+            year++;
+        }
+    }
+    for (var key in dict) {
+        if (dict[key] > max) {
+            var lyear = key;
+            max = dict[key];
+        }
+    }
+    return lyear
+}
+
+console.log(livingPeople([{ 'name': "Person1", 'birth': 1908, 'death': 1909 }, { 'name': "Person2", 'birth': 1909, 'death': 1920 }]))
+
+
+function pairSums(arr, sum) {
+    var pairs = [];
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] == sum) {
+                pairs.push([arr[i], arr[j]])
+            }
+        }
+    }
+    return pairs
+}
+
+
+function letter_num(arr) {
+    var lcount = 0;
+    var numcount = 0;
+    var max = 0;
+    var string = "";
+    var max_str = "";
+    for (var i = 0; i < arr.length - 1; i++) {
+        if (typeof arr[i] == "string") {
+            lcount += 1;
+        }
+        else {
+            numcount += 1;
+        }
+        str += arr[i]
+        for (var j = i + 1; j < arr.length; j++) {
+            str += arr[j]
+            if (typeof arr[j] == "string") {
+                lcount += 1;
+            }
+            else {
+                numcount += 1;
+            }
+            if (lcount == numcount && max < lcount + numcount) {
+                max = lcount + numcount;
+                max_str = string;
+            }
+        }
+        lcount = 0;
+        numcount = 0;
+    }
+    return max_str
+}
+
+
+function longestWord(arr) {
+    var str = "";
+    var max = "";
+    for (var i = 0; i < arr.length; i++) {
+        str = arr[i]
+        for (var j = 0; j < arr.length; j++) {
+            if (i != j) {
+                str += arr[j]
+                for (var x = 0; x < arr.length; x++) {
+                    if (x != i || x != j) {
+                        if (str == arr[x] && str.length > max.length) {
+                            max = str
+                        }
+                    }
+                }
+            }
+            str = arr[i]
+        }
+    }
+    return max
+}
+
+
+
+function circusTower(arr) {
+    var lightest = arr[0];
+    var place = 0;
+    var output = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i][0] < lightest[0] && arr[i][1] < lightest[1]) {
+            lightest = arr[i];
+            place = i;
+        }
+    }
+    arr.splice(place, 1);
+    if (arr.length > 0) {
+        output = circusTower(arr);
+    }
+    if (lightest[0] < output[output.length - 1][0] && lightest[1] < output[output.length - 1][1]) {
+        output.push(lightest)
+    }
+    return output
+}
+
+
+
+
+function stepCounter(steps) {
+    steps_remaining = steps
+    results = []
+    steps_arr = []
+    var i = 0
+    while (steps_remaining > 0) {
+        steps_arr.push(1)
+        steps_remaining -= 1
+    }
+    results.push(steps_arr)  //[[1,1,1,1]]
+    while(i < results.length){
+        //[2,1,1]
+        steps_arr = results[i]
+        for(var j = 0; j < steps_arr.length - 1; j++){
+            //[2,1,1] --> [1,1,1,1] --> [1,2,1] --> [1,1,1,1] --> [1,1,2]
+            if(steps_arr[j] == 1 && steps_arr[j+1] == 1){
+                steps_arr[j+1] = 2
+                steps_arr.splice(j,1)
+                results.push(steps_arr)
+                break
+                //[2,1,1]
+            }
+            else if(steps_arr[j] == 2){
+                steps_arr[j] = 1
+                steps_arr.splice(j,0,1)
+            }
+        }
+        i++
+    }
+    steps_arr = results[0]
+    for(var j = 0; j < steps_arr.length - 1; j++){
+        if(steps_arr[j] == 1 && steps_arr[j+1] == 1){
+            steps_arr[j+1] = 2
+            steps_arr.splice(j,1)
+        }
+    }
+    results.push(steps_arr)
+    return results
+}
+
+
+
+4
+[[1, 1, 1, 1],
+[2, 1, 1],
+[1, 2, 1],
+[1, 1, 2],
+[2, 2]]
+
+
+
+
+function multiply(arr){
+    var mult_arr = []
+    var mult = 1
+    for(var i = 0; i < arr.length; i++){
+        for(var j = 0; j < arr.length; j++){
+            if(j != i){
+                mult *= arr[j]
+            }
+        }
+        mult_arr.push(mult)
+        mult = 1
+    }
+    return mult_arr
+}
+
+
+[1,1]
+
+function nonAdjacent(arr){
+    var temp = 0
+    var place = 0
+    if(arr.length > 2){
+        var max = arr[0] + arr[arr.length - 1]
+    }
+    else{
+        return 0
+    }
+    for(var i = 0; i < arr.length; i++){
+        if(i != place + 1){
+            temp += arr[i]
+            place = i
+        }
+        if(temp > max){
+            max = temp
+        }
+    }
+    return max
+}
+
+
+
+function codeBreaker(str){
+    alpha_dict = {
+        '1': 'a',
+        '2': 'b',
+        '3': 'c',
+        '4': 'd',
+        '5': 'e',
+        '6': 'f',
+        '7': 'g',
+        '8': 'h',
+        '9': 'i',
+        '10': 'j',
+        '11': 'k',
+        '12': 'l',
+        '13': 'm',
+        '14': 'n',
+        '15': 'o',
+        '16': 'p',
+        '17': 'q',
+        '18': 'r',
+        '19': 's',
+        '20': 't',
+        '21': 'u',
+        '22': 'v',
+        '23': 'w',
+        '24': 'x',
+        '25': 'y',
+        '26': 'z'
+    }
+    var dict = {}
+    var num = ''
+    var num2 = ''
+    var num_int = 0
+    var num2_int = 0
+    var prev = ''
+    var letter = ''
+    for(var i = 0; i < str.length; i++){
+        num += str[i];
+        console.log(num)
+        num_int = parseInt(num)
+        num2_int = parseInt(num2)
+        if(num_int > 26){
+            num = str[i];
+        }
+        if(num == '0'){
+            num = ''
+        }
+        if(str[i+1] == '0'){
+            num += str[i+1];
+        }
+        else{
+          num2 = str[i] + str[i+1]
+        }
+        num2_int = parseInt(num2)
+        if(num_int > 26){
+            num2 = '';
+        }
+        prev += str[i]
+        console.log("NUM1: " + num + " NUM2: " + num2)
+        console.log("PREV: " + prev)
+    }
+    return dict[prev]
+}
+
+console.log(codeBreaker('1012233'))
+
+
+
+// 'abc' 
+// 'a' 'bc' 
+// 'a' 'cb' 
+// 'b' 'ac' 
+// 'b' 'ca' 
+// 'c' 'ab' 
+// 'c' 'ba'
+function strCombo(str){
+    var first = ''
+    var rem = ''
+    for(var i = 0; i < str.length; i++){
+        first = str[i];
+        rem = str.substring(0,i) + str.substring(i+1)
+        var perms = strCombo(rem)
+        results.push(first + perms)
+    }
+}
+
+
+
+
+
+function FirstFactorial(num) {
+    if(num == 1){
+        return num
+    }
+    else{
+        return num * FirstFactorial(num-1)
+    }
+}
+
+
+          |
+      _   |
+|    | |  |
+|   _| |  |
+|__|   |__|
+
+
+function comPool(arr){
+    var left_wall = arr[0]
+    var right_wall = 0
+    var inner = 0
+    var count = 0
+    var units = 0
+    for(var i = 1; i < arr.length; i++){
+        if(arr[i] < left_wall){
+            inner += arr[i]
+            count++
+        }
+        if(arr[i] >= left_wall){
+            units += count * left_wall - inner
+            inner = 0
+            count = 0
+            left_wall = arr[i]
+        }
+        if(arr[i] > right_wall && arr[i] < left_wall){
+            right_wall = arr[i]
+        }
+    }
+    if(right_wall < left_wall){
+        units += count * right_wall - inner
+    }
+    return units
+}
+
+race = ecarace
+car = carac
+ab = aba
+
+function subPalindrome(str, idx){
+    var count = 0
+    for(var i = idx+1; i < str.length; i++){
+
+    }
+}
+
+
+
+function inversion(arr){
+    var idx = 0;
+    var inver = 0;
+    var count = 1;
+    while(idx < arr.length){
+        if(arr[idx] > arr[idx+count]){
+            inver++;
+        }
+        if(idx+count == arr.length){
+            idx++;
+            count = 1;
+        }
+        else{
+            count++;
+        }
+    }
+    return inver
+}
+
+
+
+
+function maxProfit(arr){
+    var idx = 0
+    var count = 1
+    var max = 0
+    while(idx < arr.length){
+        if(arr[idx+count] - arr[idx] > max){
+            max = arr[idx+count] - arr[idx]
+        }
+        if(idx+count == arr.length){
+            idx++
+            count = 1
+        }
+        else{
+            count++
+        }
+    }
+    return max
+}
+
+
+
+function editDistance(str1, str2){
+    var count = 0
+    var ldelta = Math.abs(str1.length-str2.length)
+    for(var i = 0; i < str2.length; i++){
+        if (str2[i] == str1[i] || str2[i] == str1[i-ldelta] || str2[i] == str1[i+ldelta]){
+            continue
+        }
+        else{
+            count++
+        }
+    }
+    return count
+}
