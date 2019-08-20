863dac96868e6301169023552d1ff0d6ff46253d
solonburleson <solon.burleson@gmail.com>
2019-08-19 20:24:39 -0500

    MultiArr next hasnext

--- solon_august2019.js
+++ solon_august2019.js
@@ -17,3 +17,86 @@ function randomPicker(numbers, probs){
         }
     }
 }
+
+// 8/19/2019 
+class MultiArr {
+    constructor(arr){
+        this.arr = arr;
+        this.coord = null;
+    }
+
+    next(){
+        var value = null;
+        if(this.coord == null)
+        {
+            for (var i = 0; i < this.arr.length; i++) {
+                if(this.arr[i].length > 0){
+                    for(var j = 0; j < this.arr[i].length; j++){
+                        if(this.arr[i][j] != null){
+                            value = this.arr[i][j];
+                            this.coord = [i,j];
+                            return value;
+                        }
+                    }
+                }                    
+            }
+        }
+        else {
+            if(this.arr[this.coord[0]].length - 1 == this.coord[1]){
+                this.coord[0] += 1;
+                this.coord[1] = 0;
+                if(this.arr[this.coord[0]].length == 0){
+                    this.coord[0] += 1;
+                    return this.arr[this.coord[0]][this.coord[1]];
+                }
+                else{
+                    value = this.arr[this.coord[0]][this.coord[1]];
+                    return value;
+                }
+            } 
+            else{
+                value = this.arr[this.coord[0]][this.coord[1]+1];
+                this.coord[1] += 1;
+                return value;
+            }
+        }
+    }
+
+    hasnext(){
+        if(this.coord == null){
+            this.next();
+            this.hasnext();
+        }
+        if(this.arr[this.coord[0]].length - 1 > this.coord[1])
+        {
+            return true;
+        }
+        else{
+            if(this.coord[0] == this.arr.length - 1){
+                return false;
+            }
+            for(var i = this.coord[0]+1; i < this.arr.length; i++){
+                if(this.arr[i].length > 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
+
+mArr = new MultiArr([[1,2],[3],[],[4,5,6]]);
+console.log(mArr.hasnext());
+console.log(mArr.next());
+console.log(mArr.next());
+console.log(mArr.next());
+console.log(mArr.hasnext());
+console.log(mArr.next());
+console.log(mArr.hasnext());
+console.log(mArr.next());
+console.log(mArr.hasnext());
+//[1,2]
+//[3]
+//[]
+//[4,5,6]
